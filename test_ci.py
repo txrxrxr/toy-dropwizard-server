9a8ec2a62987ab41ca829755303313bcb3ec54ea
txrxrxr <xtie@ualberta.ca>
2020-01-27 18:22:23 -0700

    Added test and GH actions

--- test_ci.py
+++ test_ci.py
@@ -8,3 +8,11 @@ def test_health_check():
 # 1. Add two more tests here of your choice. I will explain the API.
 #    Make sure to verify the necessary info, e.g., status code, response data.
 # 2. Add "pytest" as integration testing script as part of Github Actions CI workflow (you've done this before!)
+
+def test_status_code():
+    r = requests.get('http://localhost:8085/hello-world?name=txr')
+    assert(r.status_code == 200)
+
+def test_content():
+    r = requests.get('http://localhost:8085/hello-world?name=txr')
+    assert(r.json()['content'] == 'Hello, txr!')
